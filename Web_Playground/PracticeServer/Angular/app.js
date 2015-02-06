ad09ca36152f65f950a8aba1dd8a23b122aeecfb
Leonardo Lee <lee.leonardo.king@gmail.com>
2015-02-05 20:45:08 -0800

    Worked a bit more on Angular stuff, might work on some more later, but I have to learn some node

--- Web_Playground/PracticeServer/Angular/app.js
+++ Web_Playground/PracticeServer/Angular/app.js
@@ -3,49 +3,80 @@
   app.controller('LearnController', function () {
     this.users = users;
   });
+  app.controller('TabController', function () {
+    this.tab = 1;
+    this.selectedTab = function ( select ) {
+      this.tab = select;
+    };
+    this.isSelected = function ( select ) {
+      return this.tab === select;
+    };
+  });
 
   var users = [
     {
       firstName: "Leo",
       lastName: "Lee",
       gender: "Male",
-      age: 24
+      age: 24,
+      description: "",
+      hobbies: [],
+      skills: [],
+      nickname: ""
     },
     {
       firstName: "Rebekah",
       lastName: "Lester",
       gender: "Female",
-      age: 22
+      age: 22,
+      description: "",
+      hobbies: [],
+      nickname: ""
     },
     {
       firstName: "Ben",
       lastName: "Duyker",
       gender: "Male",
-      age: 16
+      age: 16,
+      description: "",
+      hobbies: [],
+      nickname: ""
     },
     {
       firstName: "Richard",
       lastName: "Lee",
       gender: "Male",
-      age: 20
+      age: 20,
+      description: "",
+      hobbies: [],
+      nickname: ""
     },
     {
       firstName: "Alex",
       lastName: "Lee",
       gender: "Male",
-      age: 23
+      age: 23,
+      description: "",
+      hobbies: [],
+      nickname: ""
     },
     {
       firstName: "Mario",
       lastName: "Kart",
       gender: "Female",
-      age: 64
+      age: 64,
+      description: "",
+      hobbies: [],
+      nickname: ""
     },
     {
       firstName: "Misubishi",
       lastName: "Lancer",
       gender: "Female",
-      age: 2
+      age: 2,
+      description: "",
+      hobbies: [],
+      nickname: ""
     }
   ];
 })();
