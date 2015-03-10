6d5b6e612eb9ecef33e39a1c58c1b71c632b8881
Leonardo Lee <lee.leonardo.king@gmail.com>
2015-03-09 23:22:47 -0700

    Debating whether this is a good idea at the moment, I might as well utilize the last one and work with it.

---
+++ iOS_Playground/MotionDesignBook/MotionDesignBook/JNWAlertExample.h
@@ -0,0 +1,13 @@
+//
+//  JNWAlertExample.h
+//  MotionDesignBook
+//
+//  Created by Leonardo Lee on 3/9/15.
+//  Copyright (c) 2015 Leonardo Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JNWAlertExample : UIViewController
+
+@end
---
+++ iOS_Playground/MotionDesignBook/MotionDesignBook/JNWAlertExample.m
@@ -0,0 +1,31 @@
+//
+//  JNWAlertExample.m
+//  MotionDesignBook
+//
+//  Created by Leonardo Lee on 3/9/15.
+//  Copyright (c) 2015 Leonardo Lee. All rights reserved.
+//
+
+#import "JNWAlertExample.h"
+
+@interface JNWAlertExample ()
+
+@end
+
+@implementation JNWAlertExample
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
