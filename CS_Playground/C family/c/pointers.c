e28afadb3e14438d2bd63808d11307f4be8c9bd2
Leonardo Lee <lee.leonardo.king@gmail.com>
2015-06-27 09:49:49 -0700

    This is some pointer arithmetic and stuff, pointers are crucial in understanding arrays.

--- CS_Playground/C family/c/pointers.c
+++ CS_Playground/C family/c/pointers.c
@@ -1,12 +1,69 @@
+/*
+  Pointers are important because they alow you to actually change values.
+  Typically when a value is passed into a function, it is copied.
+  When pointers are use the values are passed in.
+  This is important because of memory management as well.
+*/
+
 #include <stdio.h>
 
+void basicPointers();
 int findString(char *input, char *find);
+void pointersAndArrays();
 
  int main(void) {
 
 
    return 0;
  }
+
+void basicPointers() {
+  int x = 1;
+  int y = 2;
+  int z[10];
+  int *pointer; //Pointer
+
+  pointer = &x; //Pointer points to x the value of 1
+  y = *pointer; //y is equal to the value of x aka 1, this copies the value to thie variable.
+  *pointer = 0; //pointer now points to 0
+
+  //THis creates an address variable (ie this creates a pointer)
+  pointer = &z[0]; //pointer now points to z[0]
+
+}
+
+void additionalPointers() {
+  int x = 10;
+  int *pointer;
+  int y;
+
+  pointer = &x; //Initializes the references itself as an address to x.
+  y = *pointer + 1; //This takes x and then assigns the value of x+1 to y.
+  *pointer = *pointer + 10; //This is going to increment the value of x by 10?
+
+  *pointer += 1; //Increments the address that pointer points to
+
+  //Interesting cases because of operator binding.
+  ++*pointer; //Same thing.
+  (*pointer)++; //This does the same thing as above, the parentheses are because of binding.
+}
+
+void pointersAndArrays() {
+  int a[10];
+  int *pa;
+  int x = 1;
+
+  pa = &a[0]; //pa now points to a[0]
+  x = *pa; //copy!
+  x = *(pa+1); //This points to a[0]
+
+  //Arrays a literal references to the 0 position of the array.
+
+  int i = 0;
+  int same = (a[i] == *(a+i)); //This is a bool.
+  //Interestingly enough a[i] is converted to *(a+i)
+
+}
 
 int findString(char *input, char *find) {
 
