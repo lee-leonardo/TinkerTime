0cdc4d901fd9ac9864fb04ec39403905d96b27db
Leonardo Lee <lee.leonardo.king@gmail.com>
2015-06-28 08:22:38 -0700

    This is adding some cleanup and making the example array and pointer comparison more understandable. I think I'll write a blog article on this as it is something that could be improved.

--- CS_Playground/C family/c/pointers.c
+++ CS_Playground/C family/c/pointers.c
@@ -8,11 +8,14 @@
 #include <stdio.h>
 
 void basicPointers();
-int findString(char *input, char *find);
+void additionalPointers();
 void pointersAndArrays();
+// int findString(char *input, char *find);
 
  int main(void) {
-
+   basicPointers();
+   additionalPointers();
+   pointersAndArrays();
 
    return 0;
  }
@@ -21,13 +24,13 @@ void basicPointers() {
   int x = 1;
   int y = 2;
   int z[10];
-  int *pointer; //Pointer
+  int *pointer;     //Pointer
 
-  pointer = &x; //Pointer points to x the value of 1
-  y = *pointer; //y is equal to the value of x aka 1, this copies the value to thie variable.
-  *pointer = 0; //pointer now points to 0
+  pointer = &x;     //Pointer points to x the value of 1
+  y = *pointer;     //y is equal to the value of x aka 1, this copies the value to thie variable.
+  *pointer = 0;     //pointer now points to 0
 
-  //THis creates an address variable (ie this creates a pointer)
+  //This creates an address variable (ie this creates a pointer)
   pointer = &z[0]; //pointer now points to z[0]
 
 }
@@ -37,15 +40,14 @@ void additionalPointers() {
   int *pointer;
   int y;
 
-  pointer = &x; //Initializes the references itself as an address to x.
-  y = *pointer + 1; //This takes x and then assigns the value of x+1 to y.
+  pointer = &x;             //Initializes the references itself as an address to x.
+  y = *pointer + 1;         //This takes x and then assigns the value of x+1 to y.
   *pointer = *pointer + 10; //This is going to increment the value of x by 10?
 
-  *pointer += 1; //Increments the address that pointer points to
-
-  //Interesting cases because of operator binding.
-  ++*pointer; //Same thing.
-  (*pointer)++; //This does the same thing as above, the parentheses are because of binding.
+  //These are interesting cases because of operator binding.
+  *pointer += 1;            //Increments the address that pointer points to.
+  ++*pointer;               //Same thing.
+  (*pointer)++;             //This does the same thing as above, the parentheses are because of binding.
 }
 
 void pointersAndArrays() {
@@ -53,23 +55,24 @@ void pointersAndArrays() {
   int *pa;
   int x = 1;
 
-  pa = &a[0]; //pa now points to a[0]
-  x = *pa; //copy!
-  x = *(pa+1); //This points to a[0]
+  pa = &a[0];                   //pa now points to a[0]
+  x = *pa;                      //copy!
+  x = *(pa+1);                  //This points to a[0]
 
   //Arrays a literal references to the 0 position of the array.
-
   int i = 0;
-  int same = (a[i] == *(a+i)); //This is a bool.
-  //Interestingly enough a[i] is converted to *(a+i)
-
-}
-
-int findString(char *input, char *find) {
-
-  int i = 0;
-  while ((*find[i]++ = *input[i]++) != '\0') {
-
+  int arrayAccess = a[i];
+  int pointerAccess =*(a+i);  //This is a bool.
+  //Interestingly enough a[i] is converted to *(a+i) with array subscripting
+  if (arrayAccess == pointerAccess) {
+    println("Cool");
   }
 
 }
+
+// int findString(char *input, char *find) {
+//
+//   int i = 0;
+//   while ((*find[i]++ = *input[i]++) != '\0') {
+//   }
+// }
