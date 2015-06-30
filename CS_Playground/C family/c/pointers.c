2adb80d395c258b8ad41b7764e4026e23bbe7a1e
Leonardo Lee <lee.leonardo.king@gmail.com>
2015-06-29 23:40:44 -0700

    This is another example. Pretty awesome what I'm learning, I'm pretty sure I'll need another book to get down the patterns.

--- CS_Playground/C family/c/pointers.c
+++ CS_Playground/C family/c/pointers.c
@@ -10,6 +10,8 @@
 void basicPointers();
 void additionalPointers();
 void pointersAndArrays();
+int lengthOfString(char *string);
+int substringLength(char *start, char *charToFind);
 // int findString(char *input, char *find);
 
  int main(void) {
@@ -86,4 +88,51 @@ int substringLength(char *start, char *charToFind) {
     loc++;
   }
   return loc - start; // This is possible because both loc and start are positions in a string, like an address.
+}
+
+/*
+  This capitalizes on how C does evaluation of operators.
+  1. To resolve the conditional of the loop
+  1.1. The internal parethentical scope will resolve first.
+  1.1.1. The left side is found and awaits assignment.
+  1.1.2. The value of the right side is found.
+  1.1.3. The right side value is then assigned to the left.
+  1.2. The conditional is evaluated.
+  2. Loop continutes.
+*/
+// s = t would copy the pointer (the address to the start of t), not the string itself.
+// The reason why we can assign these values is because we are not copying but passing in the values via atheir address.
+void stringCopy(char *newPointer, char *oldString) {
+  // Array version
+  int iterator = 0;
+  while ((newPointer[iterator] = oldString[iterator]) != '\0') {
+    iterator++;
+  }
+
+  // Pointer version
+  while ((*newPointer = *oldString) != '\0') {
+    oldString++;
+  }
+
+  //Terse version
+  while ((*newPointer++ = *oldString++) != '\0');
+
+  //Super terse, works because '\0' is the null terimination string.
+  while ((*newPointer++ = *oldString++));
+
+}
+
+void stringComparison(char *newPointer, char *oldPointer) {
+  int iterator = 0, sum = 0;
+
+  // Array version
+  for (iterator = 0; newPointer[iterator] == oldPointer[iterator]; iterator++) {
+    if (newPointer[iterator] == '\0') {
+      sum = 0;
+    }
+  }
+  sum = newPointer[iterator] - oldPointer[iterator];
+
+  //Poitner version
+
 }
