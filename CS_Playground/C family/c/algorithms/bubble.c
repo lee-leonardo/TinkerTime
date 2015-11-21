4d07539c77e8fd08fda43fc5fd7474b295398631
Leonardo Lee <lee.leonardo.king@gmail.com>
2015-11-20 21:48:31 -0800

    This is the update version that I fixed up and polished. It's meh, I still have an exception that I handle, but it's much cleaner. On my new reboot I am adding the correct ownership.

--- CS_Playground/C family/c/algorithms/bubble.c
+++ CS_Playground/C family/c/algorithms/bubble.c
@@ -2,27 +2,46 @@
 
 void Swap (int * left, int * right);
 void Bubble(int arrayToSort[], const int length);
-int LGreaterThanR(const int left, const int right);
-void PrintContents(int *arrPtr);
+int  LGreaterThanR(const int left, const int right);
+void PrintContentsWithArray(int *arrPtr, const int length);
+void PrintContentsWithPointer(int *arrPtr);                   //TODO - Not working properly... continues on looking at out of bounds
 
 int main (void) {
-  int array[] = {};
+  printf("Start of Bubble Sort testing.\n");
+  int array[] = {8,7,6,5,4,3,2,1};
   int arrayLength = sizeof(array) / sizeof(array[0]);
 
-  PrintContents(&array[0], arrayLength);
+  PrintContentsWithArray(&array[0], arrayLength);
+  // PrintContentsWithPointer(&array[0]);
   Bubble(array, arrayLength);
-  PrintContents(&array[0], arrayLength);
+  PrintContentsWithArray(&array[0], arrayLength);
+  // PrintContentsWithPointer(&array[0]);
 
   return 0;
 }
 
-// Version 1. I am working on a more efficient version.
+/*
+Bubble Sort
+Pseudocode
+  for i = 1:n,
+      swapped = false
+      for j = n:i+1,
+          if a[j] < a[j-1],
+              swap a[j,j-1]
+              swapped = true
+      → invariant: a[1..i] in final position
+      break if not swapped
+  end
+*/
 void Bubble(int arrayToSort[], const int length) {
   for (int i = 0; i < length; i++) {
-    int * last;
     for (int j = 0; j < length; j++) {
-      int * next;
-      if (LGreaterThanR(*last, *next)) Swap(last, next);
+      if (0 == j) continue;                                   /* This algorithm doesn't work when j is 0 */
+
+      int * left = &arrayToSort[(j-1)];
+      int * right = &arrayToSort[j];
+
+      if (LGreaterThanR(*left, *right)) Swap(left, right);
     }
   }
 }
@@ -39,10 +58,25 @@ int LGreaterThanR(const int left, const int right) {
   return 0;
 }
 
-void PrintContents(int *arrPtr) {
-  while (*arrPtr != '\0') {
-    if (i < (length - 1)) printf("%i, ", *arrPtr);
-    if (i < (length - 1)) printf("%i\n", *arrPtr);
-    *arrPtr++;
+void PrintContentsWithArray(int *arrPtr, const int length) {
+  printf("Printing Contents: ");
+  for (int i = 0; i < length; i++) {
+    if (i < (length - 1)) {
+      printf("%i, ", *arrPtr);
+    }
+    else if (i < (length)) {
+      printf("%i\n", *arrPtr);
+    }
+    arrPtr++;
   }
+  printf("\n");
+}
+
+void PrintContentsWithPointer(int *arrPtr) {
+  printf("Printing Contents: ");
+  while ('\0' != *arrPtr) {
+    printf("%i, ", *arrPtr);
+    arrPtr++;
+  }
+  printf("\n");
 }
